96bebd7c0dbd33bdcda302da89b01a354149590f
rajanchettri <rajan_chettri@live.com>
2024-01-30 16:07:53 +0530

    second

--- App1.java
+++ App1.java
@@ -2,5 +2,6 @@ public class App1{
 
     public static void main(String[] args){
         System.out.println("Java Brought to you By Jenkins");
+        System.out.println("GIT SCM Polling Worked!");
     }
 }
